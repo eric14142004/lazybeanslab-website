1803c847747f6a3f86d09d1e8b97657b0fb56515
ericchen <eric14142004@gmail.com>
2026-04-01 02:19:15 -0700

    test jekyll

---
+++ contact.md
@@ -0,0 +1,8 @@
+---
+layout: default
+title: 聯絡我們
+---
+
+<h2>聯絡我們</h2>
+<p>Email: info@example.com</p>
+<p>Phone: +886 900 000 000</p>
--- main.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('clickBtn').addEventListener('click', function() {
-    document.getElementById('message').textContent = "你剛剛點了按鈕！";
-});
