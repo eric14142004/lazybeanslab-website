f1d4b901dfea7fada80dc521896d3cc155954088
eric14142004 <eric14142004@gmail.com>
2026-04-10 16:01:35 -0700

    Delete .next/dev directory

--- .next/dev/static/chunks/pages_index_07xvfw~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_compiled_0by8e_8._.js","static/chunks/node_modules_next_dist_shared_lib_03mqct0._.js","static/chunks/node_modules_next_dist_client_0khy5ar._.js","static/chunks/node_modules_next_dist_0k8zr~w._.js","static/chunks/node_modules_next_0pbwiuf._.js","static/chunks/node_modules_react-dom_0bruynb._.js","static/chunks/node_modules_11l9xip._.js","static/chunks/[root-of-the-server]__09wel0u._.js"],
-    source: "entry"
-});
