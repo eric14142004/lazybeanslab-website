668d7e21638418272db920f2a951871c35b9c39a
ericchen <eric14142004@gmail.com>
2026-04-10 17:59:13 -0700

    test next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <header className="fixed top-0 left-0 w-full h-16 z-50">
+                Header
+            </header>
+
+            <main className="pt-16">
+                <Component {...pageProps} />
+            </main>
+        </>
+    );
 }
